7f43a49462ad2d287de175ed25244bb20d1775d8
RuiOkazaki <okazaki200226@gmail.com>
2022-02-10 02:40:11 +0900

    fix: divの綴じタグがなかった

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -42,31 +42,39 @@ export const Home: VFC = () => {
             が変更された時(関係なくても再レンダリングされる)
           </li>
         </ol>
-      <div className="space-y-2">
-        <h3 className="font-bold text-xl">useContextでのレンダリング最適化</h3>
-        <ol className="list-decimal px-8">
-          <li>state ごとにプロバイダーを作成する ( [tasks, setTasks] を区別しない為、値を引き出す用と更新用で分ける必要がある )</li>
-          <li>大規模になるにつれ、多くの複雑なプロバイダーを実装する必要があるため、リファクタリングなどはしにくい、DXが低くなる</li>
-        </ol>
-      </div>
-      <div className="space-y-2">
-        <h3 className="font-bold text-xl">ReactQuery</h3>
-        <h4 className="font-bold text-l">
-          <Link to="/fetch-a">
-            コンポーネントA 2回 [初回 fetch → データは cache で保つ。staletime
-            の期限が切れると再 fetch]
-          </Link>
-        </h4>
-        <ol className="list-decimal px-8">
-          <li>レンダリングされた時</li>
-          <li>fetch の処理が終わった時</li>
-        </ol>
-        <h4 className="font-bold text-l">
-          <Link to="/fetch-a">コンポーネントB 1回</Link>
-        </h4>
-        <ol className="list-decimal px-8">
-          <li>レンダリングされた時に cache から data を参照している</li>
-        </ol>
+        <div className="space-y-2">
+          <h3 className="font-bold text-xl">
+            useContextでのレンダリング最適化
+          </h3>
+          <ol className="list-decimal px-8">
+            <li>
+              state ごとにプロバイダーを作成する ( [tasks, setTasks]
+              を区別しない為、値を引き出す用と更新用で分ける必要がある )
+            </li>
+            <li>
+              大規模になるにつれ、多くの複雑なプロバイダーを実装する必要があるため、リファクタリングなどはしにくい、DXが低くなる
+            </li>
+          </ol>
+        </div>
+        <div className="space-y-2">
+          <h3 className="font-bold text-xl">ReactQuery</h3>
+          <h4 className="font-bold text-l">
+            <Link to="/fetch-a">
+              コンポーネントA 2回 [初回 fetch → データは cache で保つ。staletime
+              の期限が切れると再 fetch]
+            </Link>
+          </h4>
+          <ol className="list-decimal px-8">
+            <li>レンダリングされた時</li>
+            <li>fetch の処理が終わった時</li>
+          </ol>
+          <h4 className="font-bold text-l">
+            <Link to="/fetch-a">コンポーネントB 1回</Link>
+          </h4>
+          <ol className="list-decimal px-8">
+            <li>レンダリングされた時に cache から data を参照している</li>
+          </ol>
+        </div>
       </div>
     </div>
   )
