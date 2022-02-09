6ca95a98704a62e212170ab0be8908661978fbb9
RuiOkazaki <okazaki200226@gmail.com>
2022-02-09 15:36:05 +0900

    update: react-queryの詳細を追加した

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # 各レンダリング回数
 
-## ClassicalFetchA
+## ClassicalFetch
+
+### A 4 回 更新するたび fetch される
 
 1. レンダリングされた時
 2. マウントされた時に useEffect で isLoading の state を変更している
 3. データを取得し、setTasks で state を変更していて、カスタムフックで state を参照しているから
 4. 一連の処理が終わった際に isLoading の state を変更している
+
+### B 1 回
+
+1. レンダリングされた時に context から data を参照している
+
+## ReactQuery
+
+### A 2 回 初回 fetch → データは cache で保つ。期限が切れると再 fetch
+
+1. レンダリングされた時
+2. fetch の処理が終わった時
+
+### B 1 回
+
+1. レンダリングされた時に cache から data を参照している
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,28 +3,52 @@ import { Link } from 'react-router-dom'
 
 export const Home: VFC = () => {
   return (
-    <div className="w-9/12">
+    <div className="w-9/12 space-y-4">
       <h2 className="font-bold text-2xl">各レンダリング回数</h2>
-      <h3 className="font-bold text-xl">
-        <Link to="/fetch-a">ClassicalFetchA 4回</Link>
-      </h3>
-      <ol className="list-decimal px-8">
-        <li>レンダリングされた時</li>
-        <li>
-          マウントされた時に useEffect で isLoading の state を変更している
-        </li>
-        <li>
-          データを取得し、setTasks で state を変更していて、カスタムフックで
-          state を参照しているから
-        </li>
-        <li>一連の処理が終わった際に isLoading の state を変更している</li>
-      </ol>
-      <h3 className="font-bold text-xl">
-        <Link to="/fetch-b">ClassicalFetchB 1回</Link>
-      </h3>
-      <ol className="list-decimal px-8">
-        <li>レンダリングされた時にcontextからdataを参照している</li>
-      </ol>
+      <div className="space-y-2">
+        <h3 className="font-bold text-xl">ClassicalFetch</h3>
+        <h4 className="font-bold text-l">
+          <Link to="/fetch-a">
+            コンポーネントA 4回 [更新するたび fetch される]
+          </Link>
+        </h4>
+        <ol className="list-decimal px-8">
+          <li>レンダリングされた時</li>
+          <li>
+            マウントされた時に useEffect で isLoading の state を変更している
+          </li>
+          <li>
+            データを取得し、setTasks で state を変更していて、カスタムフックで
+            state を参照しているから
+          </li>
+          <li>一連の処理が終わった際に isLoading の state を変更している</li>
+        </ol>
+        <h4 className="font-bold text-l">
+          <Link to="/fetch-a">コンポーネントB 1回</Link>
+        </h4>
+        <ol className="list-decimal px-8">
+          <li>レンダリングされた時に context から data を参照している</li>
+        </ol>
+      </div>
+      <div className="space-y-2">
+        <h3 className="font-bold text-xl">ReactQuery</h3>
+        <h4 className="font-bold text-l">
+          <Link to="/fetch-a">
+            コンポーネントA 2回 [初回 fetch → データは cache
+            で保つ。期限が切れると再 fetch]
+          </Link>
+        </h4>
+        <ol className="list-decimal px-8">
+          <li>レンダリングされた時</li>
+          <li>fetch の処理が終わった時</li>
+        </ol>
+        <h4 className="font-bold text-l">
+          <Link to="/fetch-a">コンポーネントB 1回</Link>
+        </h4>
+        <ol className="list-decimal px-8">
+          <li>レンダリングされた時に cache から data を参照している</li>
+        </ol>
+      </div>
     </div>
   )
 }
