c0d0d9c9206183e6a8c82a8a639a7bf659a2591d
RuiOkazaki <okazaki200226@gmail.com>
2022-02-09 10:52:42 +0900

    update: fetch components を rooting した

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { ClassicalFetchA } from 'components/ClassicalFetchA'
+import { ClassicalFetchB } from 'components/ClassicalFetchB'
+import { StateProvider } from 'context/StateProvider'
 import { VFC } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Layout } from './components/Layout'
@@ -5,7 +8,18 @@ import { Layout } from './components/Layout'
 const App: VFC = () => {
   return (
     <BrowserRouter>
-      <Layout>App</Layout>
+      <StateProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/fetch-a">
+              <ClassicalFetchA />
+            </Route>
+            <Route exact path="/fetch-b">
+              <ClassicalFetchB />
+            </Route>
+          </Switch>
+        </Layout>
+      </StateProvider>
     </BrowserRouter>
   )
 }
